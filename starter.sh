a76f9879da4369eff255a8eb303702d834e3fb11
itsoluwatobby <oluwasamuel050@gmail.com>
2023-05-11 07:08:52 +0100

    creating conditionals

--- starter.sh
+++ starter.sh
@@ -13,8 +13,15 @@ then
   then
     read -p "Enter file name: " filename
     touch $filename | chmod a+x $filename
-    cat "console.log('Hello, Welcome')" >> $filename
-    echo "DONE"
+
+    read -p "httpserver boiler plate or You are good (Y/N): " res
+    if [ ${res^^} == "Y" ]
+    then  echo -e "const http = require("http");\n\nconst PORT = process.env.PORT || 5000\n\nserver = http.createServer((req, res) => {\n\tif (req.url == '/'){\n\t\tres.writeHead(200)\n\t\tres.end('Hello')\n\t}\n})\n\n\nserver.listen(PORT, () => console.log('server running on port: '+PORT))" >> $filename
+    echo "run script"
+    else
+        echo "console.log('Hello, Welcome')" >> $filename
+        echo "DONE"
+    fi
   else
     touch index.js
     echo -e "Default node project or use [--yes(-y)] flag? (Y/N): \c"
@@ -22,5 +29,5 @@ then
   fi
 
 else
-  npm init -y
+  exit
 fi
