a1a876958eec1dba1d491e8491cf1d9fb398a26e
itsoluwatobby <oluwasamuel050@gmail.com>
2023-05-11 07:36:40 +0100

    checks for conficting directory name

--- starter.sh
+++ starter.sh
@@ -4,30 +4,42 @@ read response
 
 if [ ${response^^} == "Y" ]
 then
-  echo -e "Give your directory a name: \c"
+  echo -e "Directory name: \c"
   read dirname
-  mkdir $dirname && cd $dirname
-  echo -e "Entry point file or use index.js? (Y/N): \c"
-  read dic
-  if [ ${dic^^} == "Y" ]
-  then
-    read -p "Enter file name: " filename
-    touch $filename | chmod a+x $filename
+  count=0
 
-    read -p "httpserver boiler plate or You are good (Y/N): " res
-    if [ ${res^^} == "Y" ]
-    then  echo -e "const http = require("http");\n\nconst PORT = process.env.PORT || 5000\n\nserver = http.createServer((req, res) => {\n\tif (req.url == '/'){\n\t\tres.writeHead(200)\n\t\tres.end('Hello')\n\t}\n})\n\n\nserver.listen(PORT, () => console.log('server running on port: '+PORT))" >> $filename
-    echo "run script"
-    else
-        echo "console.log('Hello, Welcome')" >> $filename
-        echo "DONE"
-    fi
+  # CHECKS FOR CONFLICTING DIRECTORY NAME
+  while [ -e $dirname -a $count -lt 3 ]
+  do  echo "Directory exits, use a new file name"
+      count=$((count + 1))
+      read -p "New directory name: " dirname
+  done
+  if [ $count -eq 3 ]
+  then echo -e 'You need to enter a new Directory name\n'
   else
-    touch index.js
-    echo -e "Default node project or use [--yes(-y)] flag? (Y/N): \c"
-    read decision
-  fi
+    mkdir $dirname && cd $dirname
+    echo -e "Entry point file or use index.js? (Y/N): \c"
+    read decide
+    if [ ${decide^^} == "Y" ]
+    then
+      read -p "Enter file name: " filename
+      touch $filename | chmod a+x $filename
 
+      # --- CREATING A DEFAULT HTTP SERVER ----
+      read -p "httpserver boiler plate or You are good (Y/N): " res
+      if [ ${res^^} == "Y" ]
+      then  echo -e "const http = require("http");\n\nconst PORT = process.env.PORT || 5000\n\nserver = http.createServer((req, res) => {\n\tif (req.url == '/'){\n\t\tres.writeHead(200)\n\t\tres.end('Hello')\n\t}\n})\n\n\nserver.listen(PORT, () => console.log('server running on port: '+PORT))" >> $filename
+      echo "run script"
+      else
+          echo "console.log('Hello, Welcome')" >> $filename
+          echo "DONE"
+      fi
+    else
+      touch index.js
+      echo -e "Default node project or use [--yes(-y)] flag? (Y/N): \c"
+      read decision
+    fi
+  fi
 else
   exit
 fi
